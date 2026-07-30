23a09325636192ac750a2bd3f99da012dbdc126a
Dawit Birhanu <dawitbirhanu2323@gmail.com>
2026-07-30 14:06:38 +0300

    Add SSL configuration to database connection

--- BackEnd/Config/dbconfig.js
+++ BackEnd/Config/dbconfig.js
@@ -7,6 +7,10 @@ const dbConfig = {
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
   port: process.env.DB_PORT,
+
+  ssl: {
+    rejectUnauthorized: true
+  }
 };
 
 const pool = mysql.createPool(dbConfig);
