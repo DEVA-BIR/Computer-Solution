15d2f7c654c52bbc49cd6772a7ccf0d151d97781
Dawit Birhanu <dawitbirhanu2323@gmail.com>
2026-08-08 14:32:22 +0300

    Refactor device routes to include additional endpoints

--- BackEnd/Routes/device.routes.js
+++ BackEnd/Routes/device.routes.js
@@ -2,9 +2,20 @@ const express = require("express");
 const router = express.Router();
 
 const deviceController = require("../Controllers/device.controller");
-router.get("/:customer_id", deviceController.getdevicesByCustomer);
-router.delete("/:id", deviceController.deletedevice);
 
+// Create
 router.post("/", deviceController.createdevice);
+
+// Get single device
+router.get("/single/:id", deviceController.getSingleDevice);
+
+// Get devices by customer
+router.get("/customer/:customer_id", deviceController.getdevicesByCustomer);
+
+// Update
+router.put("/:id", deviceController.updateDevice);
+
+// Delete
+router.delete("/:id", deviceController.deletedevice);
 
 module.exports = router;
